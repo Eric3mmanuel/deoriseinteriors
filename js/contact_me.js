86d152f6fca23a73a0e2a66f9b6909f4e7fccfe1
Eric Emmanuel <115712268+Eric3mmanuel@users.noreply.github.com>
2025-05-11 23:11:21 +0300

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "/api/contact",
+        url: ""https://api.web3forms.com/submit",
         type: "POST",
         data: {
           name: name,
